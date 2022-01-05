a5117ea7e3d17bed15ff32a762629f633ae41995
Dylan Neve <dylanneve1@gmail.com>
2022-01-05 19:41:10 +0000

    Revert "universal9810: rootdir: Convert all files into build packages"
    This reverts commit 060081f35d0ab6517fafd02c38977c8acbb18e37.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,46 +25,6 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.samsung.bsp.rc
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.samsung.bsp.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.gsp.rc
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.gps.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.baseband.rc
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.baseband.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := mobicore.rc
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/mobicore.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := ueventd.rc
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/ueventd.rc
-LOCAL_MODULE_PATH       := $(TARGET_COPY_OUT_VENDOR)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE            := init.samsungexynos9810.rc
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
