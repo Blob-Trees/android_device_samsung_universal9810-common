53fa966ab4b58dea756bd6f39cca77bbf899cbf7
Dylan Neve <dylanneve1@gmail.com>
2022-01-21 02:29:14 +0000

    SamsungDAP: Remove EFFECT_PARAM_STEREO_WIDENING_DISTANCE
    [TEST] SamsungDAP: Add initial Tile service
    
    SamsungDAP: Publicise some values that are needed for Tile service
    
    SamsungDAP: Redefine audioEffect for Tile Service instead
    
    !fixup: SamsungDAP: Link Tile service and rename main class DolbyCore
    
    SamsungDAP: Make audioEffect public
    
    SamsungDAP: Clean-up
    
    SamsungDAP: Fix build error
    
    SamsungDAP: Clean-up
    
    universal9810: Fix AndroidManifest error
    
    SamsungDAP: AndroidManifest: Correct reciever
    
    SamsungDAP: AndroidManifest: Set receiver with android:exported

--- apps/SamsungDAP/src/com/samsung/dap/DolbyCore.kt
+++ apps/SamsungDAP/src/com/samsung/dap/DolbyCore.kt
@@ -5,16 +5,18 @@
 
 package com.samsung.dap
 
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.audiofx.AudioEffect
 import android.util.Log
 
-import java.util.UUID
+import java.util.*
 
-class BootCompletedReceiver : BroadcastReceiver() {
-    private val audioEffect = AudioEffect(
+class DolbyCore : BroadcastReceiver() {
+    public val audioEffect = AudioEffect(
         EFFECT_TYPE_DAP, AudioEffect.EFFECT_TYPE_NULL, 0, 0
     )
 
@@ -26,17 +28,16 @@ class BootCompletedReceiver : BroadcastReceiver() {
     }
 
     companion object {
-        private const val TAG = "SamsungDAP"
+        public const val TAG = "SamsungDAP"
 
-        private val EFFECT_TYPE_DAP = UUID.fromString("46d279d9-9be7-453d-9d7c-ef937f675587")
+        public val EFFECT_TYPE_DAP = UUID.fromString("46d279d9-9be7-453d-9d7c-ef937f675587")
 
-        private const val EFFECT_PARAM_PROFILE = 0
-        private const val EFFECT_PARAM_STEREO_WIDENING_DISTANCE = 1
-        private const val EFFECT_PARAM_EFF_ENAB = 19
+        public const val EFFECT_PARAM_PROFILE = 0
+        public const val EFFECT_PARAM_EFF_ENAB = 19
 
-        private const val PROFILE_AUTO = 0
-        private const val PROFILE_MOVIE = 1
-        private const val PROFILE_MUSIC = 2
-        private const val PROFILE_VOICE = 3
+        public const val PROFILE_AUTO = 0
+        public const val PROFILE_MOVIE = 1
+        public const val PROFILE_MUSIC = 2
+        public const val PROFILE_VOICE = 3
     }
 }
