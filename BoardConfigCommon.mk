43631cd3468e09365ba914ef24d85ab0a0689ccc
Dylan Neve <dylanneve1@gmail.com>
2022-01-27 14:52:33 +0000

    Clean-up
    * Pretty sure we dont need private dir either

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,8 +114,6 @@ BOARD_SEPOLICY_TEE_FLAVOR := mobicore
 # SELinux SLSI
 include device/samsung_slsi/sepolicy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Soong
 BUILD_BROKEN_DUP_RULES := true
