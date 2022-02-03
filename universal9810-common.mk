6959014d3d575ed291356e6d8630fa4075424380
Dylan Neve <dylanneve1@gmail.com>
2022-02-03 19:07:35 +0000

    universal9810: vibrator: Switch back to old HAL

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -376,7 +376,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.samsung
+    android.hardware.vibrator@1.0-service.samsung-haptic
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
